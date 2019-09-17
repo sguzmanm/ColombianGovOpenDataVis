f8b568ccc06a17eec1313e031cf1bead8a59c012
Checho <s.guzmanm98@gmail.com>
2019-09-17 17:11:38 -0500

    Change visualizator since it does not xists

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -188,7 +188,7 @@ class App extends Component{
     // Render
     return(
       <div className="App">
-        <h1>Colombian Government Open Data Visualizator</h1>
+        <h1>Colombian Government Open Data Visualizer</h1>
         <p>Hi, for this prototype all you need to do is input an API link from: <a href="https://datos.gov.co/">Datos Abiertos</a> in the first field</p>
         <p>If you want to get meaningful stats from  any numeric fields please fill the second field with the name of that component in the data</p>
         <div className="container-fluid">
