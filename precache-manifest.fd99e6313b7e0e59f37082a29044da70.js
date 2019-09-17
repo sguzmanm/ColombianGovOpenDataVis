e3deea83da571a957bb289bb4d5fead353af1ca2
Checho <s.guzmanm98@gmail.com>
2019-09-17 15:57:49 -0500

    Updates

---
+++ precache-manifest.fd99e6313b7e0e59f37082a29044da70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0edcd98e790558aadd562c1efb55cbdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cc048a5bb2081cfd383",
+    "url": "/static/css/main.7d03f15d.chunk.css"
+  },
+  {
+    "revision": "3fc12f6abb0e20962a29",
+    "url": "/static/js/2.49a34e06.chunk.js"
+  },
+  {
+    "revision": "9cc048a5bb2081cfd383",
+    "url": "/static/js/main.00f8dde3.chunk.js"
+  },
+  {
+    "revision": "d0e05eca174d266a970f",
+    "url": "/static/js/runtime~main.3df769b5.js"
+  }
+]);
