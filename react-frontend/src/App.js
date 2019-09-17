170c428330c01536f3156f94747dd90f838356f5
Checho <s.guzmanm98@gmail.com>
2019-09-17 13:55:43 -0500

    Added missing href

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Colombian Government Open Data Visualizator</h1>
-      <p>Hi, for this prototype all you need to do is input an API link from: <a href="">Datos Abiertos</a> in the first field</p>
+      <p>Hi, for this prototype all you need to do is input an API link from: <a href="https://datos.gov.co/">Datos Abiertos</a> in the first field</p>
       <p>If you want to get meaningful stats from  any numeric fields please fill the second field with the name of that component in the data</p>
       {<Component1/>}
     </div>
